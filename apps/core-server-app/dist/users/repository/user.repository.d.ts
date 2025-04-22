11f671ed71f7b38493d3274b533a2bc7ad4cef74
olegkrasilev <olegkrasilev@gmail.com>
2025-04-22 17:33:43 +0300

    chore(.gitingore): apply git rm -r --cached

--- apps/core-server-app/dist/users/repository/user.repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from 'src/database/prisma.service';
-import { User } from '@packages/database';
-import { CreateUserDTO } from '../dto/user.dto';
-export declare class UserRepository {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    createUser(data: CreateUserDTO): Promise<User>;
-}
--- apps/core-server-app/dist/users/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsersService } from './users.service';
-import { User } from '@packages/database';
-import { CreateUserDTO } from './dto/user.dto';
-export declare class UsersController {
-    private readonly userService;
-    constructor(userService: UsersService);
-    createUser(createUserDto: CreateUserDTO): Promise<User>;
-}
